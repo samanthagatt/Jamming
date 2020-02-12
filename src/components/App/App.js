46a4a617267dc9797c8c5555e1e4f5a4268f4777
Samantha Gatt <smfgatt@gmail.com>
2020-02-12 12:46:29 -0500

    Sets up main page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <header>
+        <h1>Ja<span>mmm</span>ing</h1>
+      </header>
+      <main>
+        
+      </main>
     </div>
   );
 }
